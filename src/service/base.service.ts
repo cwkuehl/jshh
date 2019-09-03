69b5eaf32b067ee1c62cf6d90d3554f6d154cfbe
wolfgang <wolfgang@cwkuehl.de>
2019-09-03 23:05:56 +0200

    Menü begonnen, base.service erstellt.

---
+++ src/service/base.service.ts
@@ -0,0 +1,11 @@
+
+export class BaseService {
+
+  constructor() { }
+
+  // Globale Konstanten
+  protected SU_MAXX: number = 9; // 6
+  protected SU_MAXXW: number = 3; // 3
+  protected SU_MAXYW: number = 3; // 2
+
+}
