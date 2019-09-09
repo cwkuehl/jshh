355315267c7ca3faf894213f25b3a77a7c69cbb3
wolfgang <wolfgang@cwkuehl.de>
2019-09-09 07:45:13 +0200

    Routing potentiell erweitert.

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Fz700Component } from './components/private/fz700/fz700.component';
 import { Tb100Component } from './components/diary/tb100.component';
+//import { NotFoundPageComponent } from './containers/not-found-page';
 
 const routes: Routes = [
-    {path: '', redirectTo: 'diary', pathMatch: 'full'},
-    { path: 'memos', component: Fz700Component},
-    { path: 'diary', component: Tb100Component},
+    { path: '', redirectTo: 'diary', pathMatch: 'full' },
+    { path: 'memos', component: Fz700Component },
+    { path: 'diary', component: Tb100Component },
+    // { path: '**', component: NotFoundPageComponent }
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes, {useHash: true})],
