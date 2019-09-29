0da717ac124cf4fe54884f18e538587bc4e3ae33
wolfgang <wolfgang@cwkuehl.de>
2019-09-29 21:38:44 +0200

    Aufgeräumt.

--- src/app/reducers/tbeintrag.reducer.ts
+++ src/app/reducers/tbeintrag.reducer.ts
@@ -3,7 +3,6 @@ import { TbEintrag } from './../apis'
 import * as TbEintragActions from '../actions/tbeintrag.actions'
 import * as GlobalActions from '../actions/global.actions'
 
-// Section 1
 const initialState: TbEintrag = {
   datum: new Date(),
   eintrag: 'heute',
@@ -11,10 +10,8 @@ const initialState: TbEintrag = {
   angelegtVon: 'niemand'
 }
 
-// Section 2
 export function reducer(state: TbEintrag[] = [initialState], action: TbEintragActions.Actions) {
 
-  // Section 3
   switch(action.type) {
     case TbEintragActions.SAVE_TB_EINTRAG:
       return [...state, action.payload];
