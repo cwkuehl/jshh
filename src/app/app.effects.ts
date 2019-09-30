54c7e9a65fa5e9145ed31a948ddb6142dcafede9
wolfgang <wolfgang@cwkuehl.de>
2019-09-30 15:21:44 +0200

    Nach Anmeldung Seite gewechselt.

--- src/app/app.effects.ts
+++ src/app/app.effects.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { from, empty , EMPTY, throwError, of, pipe, Observable } from 'rxjs';
+import { from, empty, EMPTY, throwError, of, pipe, Observable } from 'rxjs';
 import { tap, map, take, mergeMap, catchError, switchMap, mapTo } from 'rxjs/operators';
 
 import * as TbEintragActions from './actions/tbeintrag.actions'
 import * as GlobalActions from './actions/global.actions'
 import { DiaryService } from './services/diary.service';
 import { UserService } from './services/user.service';
+import { dispatch } from 'rxjs/internal/observable/range';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AppEffects {
-  constructor(private actions$: Actions, private diaryservice: DiaryService, private userservice: UserService) {
+  constructor(private actions$: Actions, private router: Router,
+    private diaryservice: DiaryService, private userservice: UserService) {
     // this.addTbEintrag$.subscribe(x => {
     //   //if (x instanceof TbEintragActions.AddTbEintrag) {
     //   //console.log('AppEffects addTbEintrag: xxx ' + x.payload.datum);
@@ -34,4 +37,9 @@ export class AppEffects {
     ofType(GlobalActions.LoginGlobal),
     mergeMap(a => this.userservice.loginOb(a.payload)),
   ));
+
+  loginOkGlobal$ = createEffect(() => this.actions$.pipe(
+    ofType(GlobalActions.LoginOkGlobal),
+    tap(() => this.router.navigate(['/']))
+  ), { dispatch: false });
 }
