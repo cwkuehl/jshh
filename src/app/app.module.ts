4e14fcce8b9a170d647edad774b4b1e441893e9c
wolfgang <wolfgang@cwkuehl.de>
2019-10-24 21:01:41 +0200

    IdbService entfernt.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from './app-rounting.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { EffectsModule } from '@ngrx/effects';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { EffectsModule } from '@ngrx/effects';
+import { Store, StoreModule } from '@ngrx/store';
 import { environment } from '../environments/environment';
-
+import * as GlobalActions from './actions/global.actions';
+import { AppRoutingModule } from './app-rounting.module';
+import { AppComponent } from './app.component';
+import { AppEffects } from './app.effects';
+import { AppState } from './app.state';
+import { DateComponent } from './components/comp/date/date.component';
+import { JshhDatabase } from './components/database/database';
+import { Tb100Component } from './components/diary/tb100.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { Fz700Component } from './components/private/fz700/fz700.component';
-import { Tb100Component } from './components/diary/tb100.component';
-
-import { JshhDatabase } from './components/database/database';
-import { IdbService, PrivateService } from './services';
-import { DiaryService } from './services/diary.service';
-
-import { Tb100DeactivateGuard } from './guards/diary.guard';
-import { StoreModule, Store } from '@ngrx/store';
-import * as Reducer from './reducers/reducer';
-import * as GlobalActions from './actions/global.actions';
-import { AppEffects } from './app.effects';
-import { DateComponent } from './components/comp/date/date.component';
 import { Am000Component } from './components/user/am000/am000.component';
-import { UserService } from './services/user.service';
-import { AppState } from './app.state';
 import { Am500Component } from './components/user/am500/am500.component';
+import { Tb100DeactivateGuard } from './guards/diary.guard';
+import * as Reducer from './reducers/reducer';
+import { DiaryService, PrivateService, UserService } from './services';
 
 
 @NgModule({
@@ -54,7 +48,7 @@ import { Am500Component } from './components/user/am500/am500.component';
     HttpClientModule
   ],
   providers: [
-    JshhDatabase, IdbService, DiaryService, PrivateService,
+    JshhDatabase, DiaryService, PrivateService, UserService,
     Tb100DeactivateGuard
   ],
   bootstrap: [AppComponent]
@@ -63,11 +57,9 @@ export class AppModule {
   constructor(private userservice: UserService, private store: Store<AppState>) {
     console.log(environment.production ? "Produktion" : "Entwicklung");
     // console.log('AppModule ' + this.dbservice.getId().getMilliseconds());
-    //idbservice.createDB(); // .then(e => console.log('AppModule ' + e.benutzerId));
     userservice.getParameter('UserId').then(p => { if (p != null) store.dispatch(GlobalActions.LoginOk(p.wert)); })
       .catch(e => console.error(e));
     userservice.getParameter('ReplicationServer').then(p => { if (p != null) store.dispatch(GlobalActions.SaveReplOk(p.wert)); })
       .catch(e => console.error(e));
   }
-
 }
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,3 @@
-//export { Global } from './global';
-export { IdbService } from './idb.service';
 export { DiaryService } from './diary.service';
 export { PrivateService } from './private.service';
+export { UserService } from './user.service';
