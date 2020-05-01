321f382205a0b413441b922c74062940aecf981b
wolfgang <wolfgang@cwkuehl.de>
2020-05-01 22:36:37 +0200

    Version angepasst.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   version: '1.0.0',
-  date: '2020-04-30'
+  date: '2020-05-01'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   version: 'DEV',
-  date: '2020-04-30'
+  date: '2020-05-01'
 };
 
 /*
