f1b42b4bb70ce7fffb24614bfb019d0b0b140390
wolfgang <wolfgang@cwkuehl.de>
2020-12-31 14:26:49 +0100

    Notification-Tests.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { CheckForUpdateService } from './services';
   template: `
 <div class="container">
   <!--button class="button button-primary" (click)="subscribeToNotifications()">Abonnieren</button-->
+  <button class="button button-primary" (click)="notifyMe()">Benachrichtigung</button>
   <app-menu></app-menu>
   <div *ngIf="globalError$ | async as globalError">
     <ngb-alert type="danger" (close)="clearErrorGlobal()">{{ globalError }}</ngb-alert>
@@ -34,11 +35,12 @@ export class AppComponent implements OnInit {
     private checkForUpdateService: CheckForUpdateService) {
     this.globalError$ = store.pipe(select('globalError'));
     console.log('locale: ' + locale);
-    // this.swPush.notificationClicks.subscribe(event => {
-    //   console.log('Received notification: ', event);
-    //   const url = event.notification.data.url;
-    //   window.open(url, '_self'); // _blank
-    // });
+    this.swPush.notificationClicks.subscribe(event => {
+      console.log('Received notification: ', event);
+      const url = event.notification.data.url;
+      window.open(url, '_self'); // _blank
+      // document.location;
+    });
     this.swUpdate.available.subscribe((event) => {
       console.log('Update available: ', event);
       this.updateAvailable = true;
@@ -56,7 +58,7 @@ export class AppComponent implements OnInit {
     //     }
     //   });
     // }
-    this.subscribeToNotifications();
+    // this.subscribeToNotifications();
   }
 
   clearErrorGlobal() {
@@ -71,10 +73,66 @@ export class AppComponent implements OnInit {
       .catch(err => console.error("Could not subscribe to notifications", err));
   }
 
-  sendToServer(params: any) {
+  sendToServer(params: PushSubscription) {
     console.log('Subscription received: ', params);
+    // this.showNotification();
+    // this.notifyMe();
     // Push in Google Chrome F12 Service Worker:
-    this.http.post(this.baseUrl, { notification: params }).subscribe();
+    // this.http.post(this.baseUrl, { notification: params }).subscribe();
     // {"notification":{"title":"Benachrichtigung","body":"Medikamente nehmen!","icon":"https://jshh.cwkuehl.de/assets/icons/icon-512x512.png","vibrate":[100,50,100],"data":{"url":"https://jshh.cwkuehl.de/#/memos"}}}
+  }
+
+  // showNotification() {
+  //   Notification.requestPermission(function (result) {
+  //     if (result === 'granted') {
+  //       navigator.serviceWorker.ready.then(function (registration) {
+  //         registration.showNotification('Vibration Sample', {
+  //           body: 'Buzz! Buzz!',
+  //           icon: '../images/touch/chrome-touch-icon-192x192.png',
+  //           vibrate: [200, 100, 200, 100, 200, 100, 200],
+  //           tag: 'vibration-sample'
+  //         });
+  //       });
+  //     }
+  //   });
+  // }
+
+  notifyMe() {
+    //alert("notify start");
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+    else if (window.Notification.permission === "granted") {
+      //alert("notify granted");
+      // If it's okay let's create a notification
+      this.spawnNotification("Hi there, granted!");
+      return;
+    }
+    else if (window.Notification.permission !== "denied") {
+      //alert("notify !== denied");
+      window.Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          this.spawnNotification("Hi there, granted 2!");
+        }
+      });
+    }
+    //alert("notify end");
+    this.spawnNotification("Hi there, ungranted!");
+  }
+
+  spawnNotification(title: string, body: string = null) {
+    var url = window.location.protocol + '//' + window.location.hostname + '/#/memos';
+    body = (body || title) + ' ' + url;
+    var options = {
+      body: body,
+      icon: "https://jshh.cwkuehl.de/assets/icons/icon-72x72.png",
+      data: { url: url },
+    }
+    var n = new Notification(title, options);
+    // setTimeout(function () {
+    //   var n = new Notification(title + " time", options);
+    // }, 10);
   }
 }
